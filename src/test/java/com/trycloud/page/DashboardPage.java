5cf1e0ac6b02720197ebb9040202c6cb181b958d
ayseerarslan <ayseerarslan94@gmail.com>
2023-03-06 17:57:06 -0500

    update

--- src/test/java/com/trycloud/page/DashboardPage.java
+++ src/test/java/com/trycloud/page/DashboardPage.java
@@ -1,7 +1,11 @@
 package com.trycloud.page;
 
 import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class DashboardPage {
     //Please, EVERYBODY, feel free to create Dashboard Page locators for elements!!!
@@ -9,4 +13,6 @@ public class DashboardPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    @FindBy (xpath = "//ul[@id='appmenu']//li")
+    public List<WebElement> allDashboardLinks;
 }
