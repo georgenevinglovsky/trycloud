9cc4998948e824dea607b65485b207807912385e
FaithSlade <faithwslade@gmail.com>
2023-03-08 09:39:07 -0800

    commit more

--- src/test/java/com/trycloud/runner/CukesRunner.java
+++ src/test/java/com/trycloud/runner/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",//content
         glue = "com/trycloud/step_definition",//source
         dryRun = false,
-        tags = "faith"
+        tags = "@faith"
 )
 public class CukesRunner {
 }
